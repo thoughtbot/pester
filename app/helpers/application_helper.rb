e8afed2c96c6ce7876fa53975860a5fdf7f7bd46
Christian Reuter <christian@thoughtbot.com>
2014-12-11 12:04:07 -0700

    Markup for table-list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    file = assets.find_asset(filename)
+    render file: file.pathname
+  end
 end
