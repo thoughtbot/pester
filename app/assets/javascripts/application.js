f5bd0ff35d2619f8d79b341b9bfc900f34e1e2e8
Sean Griffin <sean@thoughtbot.com>
2014-12-11 13:39:45 -0700

    Remove Angular
    We don't need it for the UI we're envisioning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require angular
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
