891532b75dbf300107b62a1a17358c3812a12b31
Grayson Wright <wright.grayson@gmail.com>
2015-07-25 18:54:49 -0700

    use HTTParty to access public github pull requests

--- lib/github_api.rb
+++ lib/github_api.rb
@@ -1,4 +1,9 @@
+require "json"
+require "httparty"
+
 class GithubApi
   def self.pull_request_status(url)
+    response = HTTParty.get(url)
+    JSON.parse(response)["state"].to_sym
   end
 end
--- spec/lib/github_api_spec.rb
+++ spec/lib/github_api_spec.rb
@@ -1,8 +1,36 @@
 require "github_api"
+require "pry"
 
 describe GithubApi do
   describe ".pull_request_status" do
-    it "returns :open for open pull requests"
-    it "returns :closed for closed pull requests"
+    it "returns :open for open pull requests" do
+      response_json = pull_request_response_json
+      response_json["state"] = "open"
+      pull_request_url = "https://api.github.com/repos/thoughtbot/suspenders/pulls/590"
+      stub_web_request(pull_request_url, response_json)
+
+      status = GithubApi.pull_request_status(pull_request_url)
+
+      expect(status).to eq(:open)
+    end
+
+    it "returns :closed for closed pull requests" do
+      response_json = pull_request_response_json
+      response_json["state"] = "closed"
+      pull_request_url = "https://api.github.com/repos/thoughtbot/suspenders/pulls/590"
+      stub_web_request(pull_request_url, response_json)
+
+      status = GithubApi.pull_request_status(pull_request_url)
+
+      expect(status).to eq(:closed)
+    end
+  end
+
+  def stub_web_request(url, json_response)
+    stub_request(:get, url).to_return(body: json_response.to_json)
+  end
+
+  def pull_request_response_json
+    JSON.parse(File.read("spec/support/github_responses/pull_request.json"))
   end
 end
