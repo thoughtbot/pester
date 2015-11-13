60c538f9d5765ca09f66db256931f85d8f13ff32
Jason Draper <jason@drapergeek.com>
2015-11-13 11:12:11 -0500

    Remove SMTP completely
    We are not emailing and don't have any current plans to email. Having to set
    those keys to blank doesn't provide any benefit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
-
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
